ae9f5a5e9aaacbd8f87c86b88e127ce15c71c6ac
kang <113992235+kangminsukdotcom@users.noreply.github.com>
2024-05-09 16:28:05 +0900

    Update script.js

--- themes/blank/static/js/script.js
+++ themes/blank/static/js/script.js
@@ -79,3 +79,17 @@ document.addEventListener('DOMContentLoaded', function() {
       }
     });
   });
+
+  const colors = [
+    '#FD9729', '#FF63DE', '#30C825', '#BDB2FF', '#fffee0'
+];
+  document.addEventListener('DOMContentLoaded', function() {
+    const speechBubbles = document.querySelectorAll('.speech-bubble');
+
+    speechBubbles.forEach(bubble => {
+        bubble.addEventListener('mouseover', () => {
+            const randomColorIndex = Math.floor(Math.random() * colors.length);
+            bubble.style.backgroundColor = colors[randomColorIndex];
+        });
+    });
+});
